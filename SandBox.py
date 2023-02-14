558fdd9467f5df6fa487ddbea09e80090686a87e
Felipe  Mateus de carvalho <felipemateusdecarvalho@MacBook-Air-de-Felipe.local>
2023-02-14 20:26:56 -0300

    delete venv from repository

--- SandBox.py
+++
@@ -1,8 +0,0 @@
-def filtro_duas_listas(lista1, lista2):
-    return [x for x in lista1 if x in lista2]
-
-lista1 = [1, 2, 3, 4, 5, 6]
-lista2 = [4, 5, 6, 7, 8, 9]
-
-resultado = filtro_duas_listas(lista1, lista2)
-print(resultado)
